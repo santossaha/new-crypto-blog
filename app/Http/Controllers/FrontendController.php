b6e8b776585c373055498b8a39dd9d7087198cee
arunead <115103254+arunead@users.noreply.github.com>
2024-08-26 20:35:30 +0530

    update

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -2,56 +2,27 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\ServicesModel;
+
 use Illuminate\Http\Request;
+use App\Models\ServicesModel;
 
 class FrontendController extends Controller
 {
-    
+
+
     public function index(){
-
-        return view("Frontend.pages.index");
+        return view('welcome');
     }
 
-    public function about(){
-
-        return view("Frontend.pages.about");
-    }
-
-    public function service(){
 
 
-        $services = ServicesModel::all();
-
-        return view("Frontend.pages.service",compact('services'));
-}
-public function service_details($slug){
-
-    $details = ServicesModel::where('slug',$slug)->first();
-
-    $another_service = ServicesModel::where('slug','<>',$slug)->get();
-
-    return view('Frontend.pages.service-details',compact('details','another_service'));
-
-
-}
-
-
-public function contact(){
+// public function asylum(){
 
 
 
-    return view("Frontend.pages.contact_us");
+// return view("Frontend.pages.asylum");
 
-}
-
-public function asylum(){
-
-
-
-return view("Frontend.pages.asylum");
-
-}
+// }
 
 
 
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\FrontendController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,16 +16,8 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Route::get("/",['as'=>'home','uses'=>'App\Http\Controllers\FrontendController@index']);
-Route::get('about',['as'=> 'about','uses'=> 'App\Http\Controllers\FrontendController@about']);
-Route::get('service',['as'=> 'service','uses'=>'App\Http\Controllers\FrontendController@service']);
-Route::get('service/{slug?}/{id?}',['as'=> 'service_details','uses'=> 'App\Http\Controllers\FrontendController@service_details']);
-Route::get('contact',['as'=> 'contact_us','uses'=> 'App\Http\Controllers\FrontendController@contact']);
-Route::get('asylum',['as'=> 'asylum','uses'=>'App\Http\Controllers\FrontendController@asylum']);
-Route::get('login',['as'=> 'user_login','uses'=> 'App\Http\Controllers\UserController@login']);
-Route::post('login-check',['as'=> 'check_login','uses'=> 'App\Http\Controllers\UserController@check_login']);
-Route::get('register',['as'=> 'user_register','uses'=> 'App\Http\Controllers\UserController@register']);
-Route::post('save-register',['as'=> 'save_register','uses'=> 'App\Http\Controllers\UserController@save_register']);
+Route::get("/",[FrontendController::class, 'index']);
+
 
 
 Route::group(['middleware'=> 'UserMiddleware'], function () {
