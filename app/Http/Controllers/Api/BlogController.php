f5748356f857d606723352c8d3386bf44b8cdf65
jiv dev <jiv@cyberclouds.com>
2024-11-05 20:46:16 +0530

    api  details

--- app/Http/Controllers/Api/BlogController.php
+++ app/Http/Controllers/Api/BlogController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\BlogsResource;
+use App\Http\Resources\RecentViews;
 use App\Models\BlogDetail;
+use App\Models\RecentViewBlogs;
 use Exception;
 use Illuminate\Http\Request;
 
@@ -23,11 +25,46 @@ class BlogController extends Controller
     }
   }
 
-  public function blogDetail(Request $request){
+  public function blog_details(Request $request, $slug ){
     try{
 
+      $blog_details =  BlogDetail::where('slug',  $slug)->first();
+
+      // recent view 
+
+      // check exsits or not 
+
+      $exsits = RecentViewBlogs::where('blog_id',$blog_details->id)->count();
+
+      if($exsits == 0){
+
+        $RecentView = new  RecentViewBlogs();
+        $RecentView->blog_id = $blog_details->id;
+        $RecentView->save();
+      }else{
+      
+        $RecentView =   RecentViewBlogs::where('blog_id',$blog_details->id)->first();
+        $RecentView->blog_id = $blog_details->id;
+        $RecentView->save();  
+      }
+      return response()->json(['status'=>'success', $blog_details]);
+
     } catch(Exception $e){
-        
+      return response()->json(['status' => 'error', 'message' => $e->getMessage()]);  
+    }
+  }
+
+
+  public function recent_view(){
+
+    try{
+
+      $recentViews = RecentViewBlogs::orderBy('blog_id','desc')->get();
+
+      $details = RecentViews::collection(  $recentViews );
+
+    }catch(Exception $e){
+      return response()->json(['status' => 'error', 'message' => $e->getMessage()]);  
     }
   }
 }
