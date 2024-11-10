033ec29f09904f9c4b9819a6311bfeef6d57162e
jiv dev <jiv@cyberclouds.com>
2024-11-10 10:39:52 +0530

    slug add

--- app/Http/Resources/ServiceResource.php
+++ app/Http/Resources/ServiceResource.php
@@ -18,6 +18,7 @@ class ServiceResource extends JsonResource
 
         return [
                 'id'=>$this->id,
+                'slug'=>$this->slug,
                 'type'=>$this->type,
                 'categories'=>$Category
 
